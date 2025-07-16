3c391145c6910844a34e05a11e5ea5295e0c534b
Thanasis Nikolopoulos <81475280+thanasisnik@users.noreply.github.com>
2025-07-16 16:10:39 +0300

    Feats: Form without react-hook-form for a more traditional implementation.

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,31 @@
 import {z} from 'zod';
-import {useState} from "react";
+import {useEffect, useState} from "react";
+import {getUsers, type User} from "../api/users.ts";
 
 
 // Χρησιμοποιούμε zod για να κάνουμε το validation. Πρώτα ορίζουμε το σχήμα.
 const formSchema = z.object({
     // Το z.email() του zod αν δεν του περάσουμε regex, το default είναι /^(?!\.)(?!.*\.\.)([a-z0-9_'+\-\.]*)[a-z0-9_+-]@([a-z0-9][a-z0-9\-]*\.)+[a-z]{2,}$/i
-    email: z.email("Please enter a valid email"),
+    email: z
+        .email("Please enter a valid email")
+        .optional()
+        .or(z.literal("")),
     subject: z
         .string()
         .nonempty("Subject is required."),
     description: z
         .string()
         .nonempty("Description is required.")
-        .min(10, "Must be at least 10 characters"),
-
-
+        .min(10, "Must be at least 10 characters")
+        .max(3000, "You have reached max length"),
 })
 
 // Σε έναν τύπο FormValues τραβάμε την πληροφορία από το zod σχήμα. Δηλαδή το infer δημιουργεί αυτόματα τον τύπο του σχήματος.
 type FormValues = z.infer<typeof formSchema>;
+
+type Props = {
+    onClose?: () => void;
+}
 
 // Για τα errors φτιάχνουμε έναν νέο τύπο, με τα πεδία του FormValues αλλά optional. Δεν είναι strict (Μπορεί να μην έχουμε κανένα, ένα ή και παραπάνω errors).
 type FormErrors = {
@@ -33,15 +40,26 @@ const initValues = {
     description: "",
 }
 
-const Form = () => {
+type SubmittedData = {
+    subject: string;
+    description: string;
+    recipients: User[];
+}
+
+const Form = ({onClose}: Props) => {
     // Χρήση της useState για να αλλάζουμε την τιμή των values. Δηλώνουμε οτι είναι τύπου FormValues και οτι έχουν μια αρχική τιμή όπως δηλώσαμε στο initValues.
     const [values, setValues] = useState<FormValues>(initValues)
 
-    // Χρήση της useState για διαχείριση του submit.
-    const [submittedData, setSubmittedData] = useState<FormValues | null>(null)
-
     // Χρήση της useState για να αλλάζουμε την τιμή των errors. Δηλώνουμε οτι είναι τύπου FormErrors.
     const [errors, setErrors] = useState<FormErrors>({})
+
+    const [users, setUsers] = useState<User[]>([]);
+    const [allUsers, setAllUsers] = useState<User[]>([]);
+    const [autoComplete, setAutoComplete] = useState<User[]>([]) // Το state για να διαχειριστούμε το suggestion list όταν ο χρήστης θέλει να εισάγει κάποιον στους παραλήπτες.
+    const [submittedData, setSubmittedData] = useState<SubmittedData | null>(null) // Το state για να εκτυπώσουμε το submit.
+    const [isLoading, setIsLoading] = useState<boolean>(false);
+
+
 
 
     const validateForm = () => {
@@ -67,12 +85,39 @@ const Form = () => {
         return true;
     };
 
+
+    // Χρήση της useEffect χωρίς dependencies. Μας επιτρέπει όταν ανοίγουμε τη σελίδα να τρέξουμε αυτό το effect όπου θα καλέσει την getUsers και έπειτα να κάνουμε set το state με τους users.
+    useEffect(() => {
+        getUsers()
+            .then(allUsers => setAllUsers(allUsers))
+    }, [])
+
+    // Χρήση της useEffect με dependencies. Τρέχει κάθε φορά που βλέπει αλλαγή στο emailInput, και φιλτράρει με το email ή το name, για να περάσει τους
+    // κατάλληλους χρήστες στο suggestion list.
+    useEffect(() => {
+        const search = values.email?.toLowerCase() ?? "";
+        const filtered = allUsers.filter(user =>
+            user.email.toLowerCase().includes(search) ||
+            user.name.toLowerCase().includes(search)
+        );
+        setAutoComplete(filtered);
+    }, [values.email, allUsers]);
+
     const handleSubmit = (e: React.FormEvent) => {
+        if (users.length === 0) {
+            setErrors((prev) => ({ ...prev, email: "Please enter at least one valid email" }));
+            return;
+        }
+
         e.preventDefault();
         const isValid = validateForm();
 
         if (isValid) {
-            setSubmittedData(values);
+            setSubmittedData({
+                subject: values.subject,
+                description: values.description,
+                recipients: users,
+            });
             setValues(initValues);
         }
     }
@@ -93,11 +138,55 @@ const Form = () => {
         setSubmittedData(null)
     }
 
+    const enterAll = async () => {
+        setIsLoading(true);
+        try {
+            const allUsers = await getUsers();
+            setUsers(allUsers);
+        } finally {
+            setIsLoading(false);
+        }
+    };
+
+    const clearAll = () => {
+        setUsers([]);
+    };
+
+    // Χρήση της συνάρτησης για να διαγράψουμε έναν user απο τη λίστα παραληπτών.
+    const onClearOne = (user: User) => {
+        setUsers(prev => {
+            const exists = prev.some( u => u.email === user.email);
+            if (!exists) return prev;
+
+            return prev.filter(u => u.email !== user.email);
+        });
+    };
+
+    // Χρήση για να διαχειριστούμε το suggestion list. Δηλαδή να μπορούμε να κλικάρουμε έναν user και αυτός να περνάει στη λίστα παραληπτών.
+    const handleAutoComplete = (user: User) => {
+        // Αρχικά ελέγχουμε αν υπάρχει ο χρήστης αλλιώς τον προσθέτουμε.
+        setUsers(prev => {
+            const exists = prev.some(u => u.email === user.email );
+            if (exists) return prev;
+            return [...prev, user];
+        })
+        setValues((prev) => ({ ...prev, email: "" }));
+        setAutoComplete([])
+    }
+
 
     return (
         <>
             <div className="container mx-auto bg-white px-4 py-2">
-                <h1 className="border-b border-gray-300 font-semibold text-2xl text-gray-700 my-4">Email sent FormWithReactHook</h1>
+                <div className="flex justify-between border-b border-gray-300 pb-2 mb-6">
+                    <h1 className=" font-semibold text-2xl text-gray-700 my-4">Email sent Form</h1>
+                    <button
+                        type="button"
+                        className="text-button-blue font-semibold p-2 "
+                        onClick={onClose}
+                    > Χ </button>
+                </div>
+
                 <form onSubmit={handleSubmit} className="min-w-3xl min-h-[70vh] flex flex-col space-y-6 ">
                     {/* Το πρώτο κομμάτι με τη λίστα παραληπτών*/}
                     <div>
@@ -105,22 +194,54 @@ const Form = () => {
                         {/* Εδώ έχουμε ενα div για να εμφανίζεται η λίστα παραληπτών*/}
                         <div className="border border-gray-500 rounded-md min-h-[50px] max-w-3xl flex flex-wrap space-x-4 space-y-2 py-2 px-1">
                             {/* Εδώ θα μπαινουν οι παραλήπτες   */}
-                            {/*    mock data*/}
-                            <p className="border border-email-border rounded border-2 py-1 px-2 text-email-blue font-semibold">thanasis@outlook.com</p>
-                            <p className="border border-email-border rounded border-2 py-1 px-2 text-email-blue font-semibold">thanasisfdfafdaf@outlook.com</p>
-                            <p className="border border-email-border rounded border-2 py-1 px-2 text-email-blue font-semibold">thanasisfdfafdaf@outlook.com</p>
-                            <p className="border border-email-border rounded border-2 py-1 px-2 text-email-blue font-semibold">thanasisfdfafdaf@outlook.com</p>
+                            {users.map((user) => (
+                                <p key={user.id} className="text-sm text-email-blue font-semibold">
+                                    {user.email}
+                                    <button type="button" onClick={() => onClearOne(user)}>✕</button>
+                                </p>
+                            ))}
 
                         </div>
 
                         <div className="flex flex-col">
-                            <input type="text" placeholder="Type name or email" name="email" onChange={handleChange} value={values.email}
-                                   className="border border-gray-500 text-gray-900 mt-1 rounded-md min-h-[40px] px-2 mt-2"
+                            <input
+                                type="text"
+                                placeholder="Type name or email"
+                                name="email" onChange={handleChange}
+                                value={values.email}
+                                className="border border-gray-500 text-gray-900 mt-1 rounded-md min-h-[40px] px-2 mt-2"
                             />
+                            {values.email && autoComplete.length > 0 && (
+                                <ul className="max-w-md max-h-[150px] py-1 px-0.5 overflow-auto scrollbar-hidden mt-2 border border-gray-300 rounded-md">
+                                    {autoComplete.map(user => (
+                                        <li
+                                            key={user.id}
+                                            onClick={() => handleAutoComplete(user)}
+                                            className="cursor-pointer border-b last:border-none bg-button-blue text-white p-2 hover:border hover:border-button-blue hover:bg-white hover:text-button-text-blue transition duration-300"
+                                        >
+                                            <span><strong>Name:</strong> {user.name}</span><br/>
+                                            <span><strong>Email:</strong> {user.email}</span>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+
 
                             <div className="space-x-4 flex mt-4">
-                                <button className="bg-button-blue rounded-lg text-white font-semibold text-sm px-3 py-2 ">ENTER ALL CUSTOMERS</button>
-                                <button className="border border-button-border-blue rounded-lg text-button-text-blue font-semibold text-sm px-3 py-2 ">REMOVE ALL CLIENTS</button>
+                                <button
+                                    type="button"
+                                    onClick={enterAll}
+                                    className="bg-button-blue rounded-lg text-white font-semibold text-sm px-3 py-2 "
+                                >
+                                    {isLoading? "Loading..." : "ENTER ALL CUSTOMERS"}
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={clearAll}
+                                    className="border border-button-border-blue rounded-lg text-button-text-blue font-semibold text-sm px-3 py-2 "
+                                >
+                                    REMOVE ALL CLIENTS
+                                </button>
                             </div>
 
                             {errors.email && (
@@ -163,11 +284,16 @@ const Form = () => {
                 </form>
 
                 {submittedData && (
-                    <div className="mt-6 border-t border-gray-500 pt-4 space-y-2">
-                        <h2 className="font-semibold text-gray-800">Data to submit</h2>
-                        <p><strong>Email:</strong> {submittedData.email}</p>
-                        <p><strong>Subject:</strong> {submittedData.subject}</p>
-                        <p><strong>Description:</strong> {submittedData.description}</p>
+                    <div>
+                        <h2 className="font-bold text-xl mt-6">Submitted Data</h2>
+                        <p><strong>Subject:</strong>{submittedData.subject}</p>
+                        <p><strong>Description</strong>{submittedData.description}</p>
+                        <h3 className="font-bold text-xl mt-2">Recipients:</h3>
+                        <ul className="list-disc list-inside text-gray-700">
+                            {submittedData.recipients.map((recipient) => (
+                                <li key={recipient.id}>{recipient.email}</li>
+                            ))}
+                        </ul>
                     </div>
                 )}
 
