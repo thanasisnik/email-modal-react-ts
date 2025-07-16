1e28842571c7c77d664e0f4eea191361415a8518
Thanasis Nikolopoulos <81475280+thanasisnik@users.noreply.github.com>
2025-07-16 16:11:37 +0300

    Feats: Dialog opens a modal with the FormWithReactHook component. Form component commented out.

--- src/components/Dialog.tsx
+++ src/components/Dialog.tsx
@@ -1,5 +1,6 @@
 import {useRef} from "react";
 import FormWithReactHook from "./FormWithReactHook.tsx";
+// import Form from "./Form.tsx";
 
 const Dialog = () => {
 
@@ -19,7 +20,11 @@ const Dialog = () => {
                 <dialog ref={dialogRef} className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
                                                  p-5 border border-blue-500 rounded bg-white w-[95vw] max-w-4xl max-h-[90vh] overflow-auto">
 
+                    {/* Χρησιμοποιούμε Props για να κλείσουμε το modal από το component. */}
                     <FormWithReactHook onClose={() => dialogRef.current?.close()} />
+
+                    {/* This is the form without react-hook-form, comment-out the next line and the import, to see this component. */}
+                    {/*<Form onClose={() => dialogRef.current?.close()} />*/}
                 </dialog>
                 <button onClick={openDialog}>Open modal</button>
             </div>
